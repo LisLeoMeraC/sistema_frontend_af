4874add82cc861eed25f49061b1eab5ff27def99
LisLeoMeraC <84941789+LisLeoMeraC@users.noreply.github.com>
2026-05-08 07:49:07 -0500

    Update helper.ts

--- src/app/demo/service/helper.ts
+++ src/app/demo/service/helper.ts
@@ -1,5 +1,5 @@
 //url
-let baseUrl = 'https://agrofranco-backend-cocoa.onrender.com';
+let baseUrl = 'https://agrofranco-backend-cocoa-ktah.onrender.com';
 
 //let baseUrl = 'http://localhost:8080';
 export default baseUrl;
